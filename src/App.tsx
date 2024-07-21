7a222d94e2f2aa1b60c1968fcc0691c7f7ed1021
Ornella B <borges.ornella@gmail.com>
2024-07-21 17:04:48 +0200

    test input

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  // INPUT STATE
+
+  const [setsCsvFile, setSetsCsvFile] = useState<File | null>(null);
+  const [inventoriesCsvFile, setInventoriesCsvFile] = useState<File | null>(
+    null
+  );
+  const [partsCsvFile, setPartsCsvFile] = useState<File | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputName = e.target.name;
+    console.log("inputName is :", inputName);
+    const file = e.target.files[0];
+    console.log("file", file);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Importez vos trois fichiers csv</h1>
+        <div className="inputGroup">
+          <label className="label" htmlFor="setFileInput">
+            Sets:
+            <input
+              type="file"
+              name="sets"
+              accept=".csv"
+              id="setFileInput"
+              style={{ display: "block", marginTop: "8px" }}
+              onChange={handleFileChange}
+            />
+          </label>
+          <label className="label" htmlFor="inventoriesFileInput">
+            Inventories:
+            <input
+              type="file"
+              name="inventories"
+              accept=".csv"
+              id="inventoriesFileInput"
+              className="inputFile"
+            />
+          </label>
+          <label className="label" htmlFor="partFileInput">
+            Parts:
+            <input type="file" name="parts" accept=".csv" id="partFileInput" />
+          </label>
+
+          {/*      <div>
+            <button>Submit</button>
+          </div> */}
+        </div>
       </header>
     </div>
   );
